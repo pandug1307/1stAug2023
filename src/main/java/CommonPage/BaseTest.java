3afb1185feaceb201892c2629d9d4a422f4dd29e
umesh.pandey <umesh.pandey@softwebsolutions.com>
2023-08-03 13:31:24 +0530

    Changes done in ExtentReport

--- src/main/java/CommonPage/BaseTest.java
+++ src/main/java/CommonPage/BaseTest.java
@@ -35,7 +35,7 @@ public class BaseTest {
 
         extentReports= new ExtentReports();
         //sparkReporter= new ExtentSparkReporter(new File(System.getProperty("user.dir")+"/Reports/Report.html"));
-        sparkReporter= new ExtentSparkReporter(new File(("http://localhost:8080/job/AlliedMavenJobPipeline/ws/Report.html")));
+        sparkReporter= new ExtentSparkReporter(new File(("http://localhost:8080/job/AlliedMavenJobPipeline/ExtentReport")));
         extentReports.attachReporter(sparkReporter);
         extentReports.setSystemInfo("Environment","QA");
 
